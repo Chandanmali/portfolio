d5a28b3a1277b2f18fce4e63f0f1d44ae0d280c8
Chandan Mali <chandanmali21117@gmail.com>
2026-05-07 13:17:09 +0530

    Updated user API route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import userModel from "./Model/userModel";
 import connectDB from "@/lib/db";
+const MONGODB_URL = process.env.MONGODB_URL;
 
 export async function POST(req: NextRequest) {
 
-  await connectDB("mongodb+srv://chandanmali21117_db_user:chandan21117@cluster0.hzefken.mongodb.net/portfolio");
+  await connectDB(MONGODB_URL);
 
   const body = await req.json();
 
