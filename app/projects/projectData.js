4fd296959b9176ec2e1177b83bcf2649aa012d12
Chandan Mali <chandanmali21117@gmail.com>
2026-05-07 13:33:38 +0530

    description added

--- app/projects/projectData.js
+++ app/projects/projectData.js
@@ -1,26 +1,26 @@
 export const projectData = [
   {
     id: 1,
-    imgp: "/pro1.png",
+    imgp: "/pro1.PNG",
     name: "Chat Web-App",
     tech: ["websocket", "mongoDB", "node", "express", "react"],
-    des: "ufu fufuwbfu wfiwfui fwijijf",
+    des: "A modern real-time chat application built for seamless and instant communication between users. Features secure messaging, responsive design, and an interactive user interface for a smooth chatting experience.",
     url: "https://chat-app-rho-ebon.vercel.app/",
   },
   {
     id: 2,
-    imgp: "/pro2.png",
+    imgp: "/pro2.PNG",
     name: "Crypto-currency-tracker",
     tech: ["websocket", "mongoDB", "node", "express", "react"],
-    des: "ufu fufuwbfu wfiwfui fwijijf",
+    des: "A modern crypto tracker application that provides real-time cryptocurrency prices, market trends, and detailed coin information. Built with a responsive and user-friendly interface to help users monitor and explore the crypto market efficiently.",
     url: "https://crypto-tracker-lime-sigma.vercel.app/",
   },
   {
     id: 3,
-    imgp: "/pro3.png",
+    imgp: "/pro3.PNG",
     name: "Note-Taking Web-App",
     tech: ["websocket", "mongoDB", "node", "express", "react"],
-    des: "ufu fufuwbfu wfiwfui fwijijf",
+    des: "A full-stack note-taking application built with MongoDB, Express.js, and modern frontend technologies for efficient note management. Features secure CRUD operations, responsive design, and a clean user interface for a smooth and organized user experience.",
     url: "https://note-taking-weld-three.vercel.app/",
   },
 ];
